1ae84e61e167fe8407506b2696255dd76aba90e4
Naguumo <ishbharal@gmail.com>
2021-05-17 14:24:14 -0500

    Completed calendar class

--- src/calendar.ts
+++ src/calendar.ts
@@ -3,11 +3,36 @@ import { CalendarType } from './types'
 import { ZonedDateTime } from './zonedDateTime'
 
 export class Calendar {
-  constructor(readonly id: CalendarType = 'iso8601') {}
+  private format
 
-  year(dt: PlainDateTime | ZonedDateTime) {}
-  month(dt: PlainDateTime | ZonedDateTime) {}
-  day(dt: PlainDateTime | ZonedDateTime) {}
-  dayOfWeek(dt: PlainDateTime | ZonedDateTime) {}
+  constructor(readonly id: CalendarType = 'iso8601') {
+    this.format = Intl.DateTimeFormat('en-us', { calendar: id })
+  }
+
+  private formattedPropertyValue(
+    dt: PlainDateTime | ZonedDateTime,
+    property: string
+  ) {
+    return this.format.formatToParts(dt.epochMilliseconds).reduce(
+      (acc: { [type: string]: string }, { type, value }) => ({
+        ...acc,
+        [type]: value,
+      }),
+      {}
+    )[property]
+  }
+
+  year(dt: PlainDateTime | ZonedDateTime) {
+    return parseInt(this.formattedPropertyValue(dt, 'year') || '1970')
+  }
+  month(dt: PlainDateTime | ZonedDateTime) {
+    return parseInt(this.formattedPropertyValue(dt, 'month') || '1')
+  }
+  day(dt: PlainDateTime | ZonedDateTime) {
+    return parseInt(this.formattedPropertyValue(dt, 'day') || '1')
+  }
+  dayOfWeek(dt: PlainDateTime | ZonedDateTime) {
+    return this.formattedPropertyValue(dt, 'weekday')
+  }
   weekOfYear(dt: PlainDateTime | ZonedDateTime) {}
 }
