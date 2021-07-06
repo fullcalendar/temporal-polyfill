f5827ac3904638bb1bf1f86ac76ee4865675ab5b
Naguumo <ishbharal@gmail.com>
2021-07-06 09:13:45 -0500

    DurationFormat complete first iteration

--- packages/temporal-format/src/durationFormat.test.ts
+++ packages/temporal-format/src/durationFormat.test.ts
@@ -2,9 +2,56 @@ import { Duration } from 'temporal-ponyfill'
 import { DurationFormat } from './durationFormat'
 
 test.each([
-  [new Duration(1), [1, 'year']],
-  [new Duration(0, 1, 0, 1), [1, 'month', 1, 'day']],
+  [
+    new Duration(1),
+    [
+      {
+        type: 'integer',
+        value: '1',
+        unit: 'year',
+      },
+      {
+        type: 'literal',
+        value: ' year',
+      },
+    ],
+  ],
+  [
+    new Duration(0, 1, 0, 1),
+    [
+      {
+        type: 'integer',
+        value: '1',
+        unit: 'month',
+      },
+      {
+        type: 'literal',
+        value: ' month',
+      },
+      {
+        type: 'literal',
+        value: ' ',
+      },
+      {
+        type: 'integer',
+        value: '1',
+        unit: 'day',
+      },
+      {
+        type: 'literal',
+        value: ' day',
+      },
+    ],
+  ],
 ])('can formatToParts %s', (dur, expected) => {
   const formatter = new DurationFormat()
   expect(formatter.formatToParts(dur)).toEqual(expected)
+})
+
+test.each([
+  [new Duration(1), '1 year'],
+  [new Duration(0, 1, 0, 1), '1 month 1 day'],
+])('can formatToParts %s', (dur, expected) => {
+  const formatter = new DurationFormat()
+  expect(formatter.format(dur)).toEqual(expected)
 })
--- packages/temporal-format/src/durationFormat.ts
+++ packages/temporal-format/src/durationFormat.ts
@@ -3,7 +3,26 @@ import { DurationLike } from 'temporal-ponyfill/dist/duration'
 import { LocaleId } from 'temporal-ponyfill/dist/utils'
 
 const largestCommonString = (a: string, b: string): string => {
-  return '' // TODO Implement this
+  // FIXME: There has to be a better way
+  let largest = ''
+
+  for (let i = 0; i <= a.length; i++) {
+    const aSub = a.substring(0, i)
+    const bSub = b.substring(0, i)
+
+    if (aSub === bSub && aSub.length > largest.length) {
+      largest = aSub
+    }
+
+    const aBackSub = a.substring(a.length - 1 - i, a.length)
+    const bBackSub = b.substring(b.length - 1 - i, b.length)
+
+    if (aBackSub === bBackSub && aBackSub.length > largest.length) {
+      largest = aBackSub
+    }
+  }
+
+  return largest
 }
 
 type DurationFormatOptions = { style: 'long' | 'short' | 'narrow' }
@@ -21,12 +40,15 @@ export class DurationFormat {
     })
   }
 
-  formatToParts(durationLike: Duration | DurationLike): Array<any> {
+  formatToParts(
+    durationLike: Duration | DurationLike
+  ): Array<Intl.RelativeTimeFormatPart> {
     const duration =
       durationLike instanceof Duration
         ? durationLike
         : Duration.from(durationLike)
 
+    // I just don't want to write this statement 4 times
     const getLiteralValue = (
       arr: Array<Intl.RelativeTimeFormatPart>,
       index: number
@@ -35,13 +57,17 @@ export class DurationFormat {
     }
 
     return Object.keys(duration).reduce(
-      (accum, key: Intl.RelativeTimeFormatUnit) => {
+      (
+        accum: Array<Intl.RelativeTimeFormatPart>,
+        key: Intl.RelativeTimeFormatUnit
+      ) => {
         const val = duration[key]
 
         if (val !== 0) {
           const forwardUnit = this.formatter.formatToParts(val, key)
           const backwardUnit = this.formatter.formatToParts(-val, key)
 
+          // Extract common string from formatted parts
           const before = largestCommonString(
             getLiteralValue(forwardUnit, 0),
             getLiteralValue(backwardUnit, 0)
@@ -50,14 +76,22 @@ export class DurationFormat {
             getLiteralValue(forwardUnit, forwardUnit.length - 1),
             getLiteralValue(backwardUnit, backwardUnit.length - 1)
           )
-          return [
-            ...accum,
-            val,
-            key.substring(0, key.length - 1),
-            // { type: 'literal', value: before},
-            // { type: 'integer', value: val, unit: key },
-            // { type: 'literal', value: after },
-          ]
+
+          // Append pretext into accumulator
+          if (before !== '' || accum.length > 0) {
+            accum.push({
+              type: 'literal',
+              value: accum.length > 0 ? ' ' : '' + before,
+            })
+          }
+
+          // Account for before part not being present
+          accum.push(forwardUnit[forwardUnit[0].type === 'literal' ? 1 : 0])
+
+          // Append posttext into accumulator
+          if (after !== '') {
+            accum.push({ type: 'literal', value: after })
+          }
         }
         return accum
       },
@@ -67,6 +101,10 @@ export class DurationFormat {
 
   format(durationLike: Duration | DurationLike): string {
     const parts = this.formatToParts(durationLike)
-    return parts.join(' ')
+    return parts
+      .map((val) => {
+        return val.value
+      })
+      .join('')
   }
 }
