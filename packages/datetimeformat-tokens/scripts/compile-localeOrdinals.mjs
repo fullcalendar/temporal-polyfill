e70e632e46b72bb637c1d16129a71b19279c9c1b
Naguumo <ishbharal@gmail.com>
2021-07-12 14:04:38 -0500

    Compressed ordinals script

--- packages/datetimeformat-tokens/scripts/compile-localeOrdinals.mjs
+++ packages/datetimeformat-tokens/scripts/compile-localeOrdinals.mjs
@@ -1,1 +1,56 @@
-// TODO: Make this
+import { writeFileSync } from 'fs'
+import { resolve } from 'path'
+import { localesReduceAsync } from '../../../scripts/lib/locales-list.mjs'
+
+const templateCode = (obj) => {
+  return `/* eslint-disable */
+
+import { Ordinals } from './ordinals'
+
+const ordinals = ${JSON.stringify(obj, null, 2)}
+
+const expandOrdinals = (): { [key: string]: Ordinals } => {
+  const obj = {}
+
+  for (const key in ordinals) {
+    const val = ordinals[key]
+
+    for (const locale of key.split('|')) {
+      obj[locale] = val
+    }
+  }
+
+  return obj
+}
+
+export const localeOrdinals = expandOrdinals()
+`
+}
+
+localesReduceAsync().then((locales) => {
+  const ordinals = {}
+
+  for (const locale in locales) {
+    const ordinal = locales[locale].ordinal
+
+    if (ordinal) {
+      const existingKey = Object.keys(ordinals).find((key) => {
+        return ordinals[key] === ordinal
+      })
+
+      if (existingKey) {
+        ordinals[`${existingKey}|${locale}`] = ordinal
+        delete ordinals[existingKey]
+      } else {
+        ordinals[locale] = ordinal
+      }
+    }
+  }
+
+  writeFileSync(resolve('src/localeOrdinals.ts'), templateCode(ordinals), {
+    encoding: 'utf8',
+    flag: 'w',
+  })
+
+  console.log('Wrote localeOrdinals.ts')
+})
--- packages/datetimeformat-tokens/src/localeOrdinals.ts
+++ packages/datetimeformat-tokens/src/localeOrdinals.ts
@@ -1,18 +1,25 @@
+/* eslint-disable */
+
 import { Ordinals } from './ordinals'
 
 const ordinals = {
-  'et|fi': '.',
-  'es|pt|pt-br': 'º',
-  en: {
-    one: 'st',
-    two: 'nd',
-    few: 'rd',
-    other: 'th',
+  "cv": "-мӗш",
+  "el": "η",
+  "en": {
+    "one": "st",
+    "two": "nd",
+    "few": "rd",
+    "other": "th"
   },
-  fr: {
-    m: 'er',
-    f: 're',
+  "eo": "a",
+  "fa": "م",
+  "fr": {
+    "m": "er",
+    "f": "re"
   },
+  "es-DO|es-MX|es-US|es|gl|it-CH|it|mi|mt|pt-BR|pt": "º",
+  "te": "వ",
+  "bs|cs|da|de-AT|de-CH|de|et|eu|fi|fo|hr|hu|is|lb|lv|me|nb|nn|pl|se|sk|sl|sq|sr-CYRL|sr|tlh|tzl": "."
 }
 
 const expandOrdinals = (): { [key: string]: Ordinals } => {
