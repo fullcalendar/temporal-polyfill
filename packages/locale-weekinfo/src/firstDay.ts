a11238e1c93b4431de00407938b8d9b113512b25
Naguumo <ishbharal@gmail.com>
2021-07-12 14:04:56 -0500

    Fix package names so there's no recursion

--- packages/locale-weekinfo/src/firstDay.ts
+++ packages/locale-weekinfo/src/firstDay.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 
 export const getFirstDay = (locale: string): number => {
-  if (locale.match(/^((?:af|ar-ma|ar-tn|az|be|bg|bm|br|bs|ca|cs|cv|cy|da|de-at|de-ch|de|el|en-gb|en-ie|en-nz|en-sg|eo|es-do|es|et|eu|fi|fil|fo|fr-ch|fr|fy|ga|gd|gl|hr|hu|hy-am|is|it-ch|it|jv|ka|kk|km|ky|lb|lt|lv|me|mi|mk|ms-my|ms|mt|my|nb|nl-be|nl|nn|oc-lnc|pl|pt|ro|ru|sd|se|sk|sl|sq|sr-cyrl|sr|ss|sv|sw|tet|tg|tk|tl-ph|tlh|tr|tzl|ug-cn|uk|ur|uz-latn|uz|vi|x-pseudo|yo|zh-cn)(?:-\w{2})?)$/)) {
+  if (locale.match(/^((?:af|ar-MA|ar-TN|az|be|bg|bm|br|bs|ca|cs|cv|cy|da|de-AT|de-CH|de|el|en-GB|en-IE|en-NZ|en-SG|eo|es-DO|es|et|eu|fi|fil|fo|fr-CH|fr|fy|ga|gd|gl|hr|hu|hy-AM|is|it-CH|it|jv|ka|kk|km|ky|lb|lt|lv|me|mi|mk|ms-MY|ms|mt|my|nb|nl-BE|nl|nn|oc-LNC|pl|pt|ro|ru|sd|se|sk|sl|sq|sr-CYRL|sr|ss|sv|sw|tet|tg|tk|tl-PH|tlh|tr|tzl|ug-CN|uk|ur|uz-LATN|uz|vi|x-PSEUDO|yo|zh-CN)(?:-\w{2})?)$/)) {
     return 2
-  } else if (locale.match(/^((?:ar-ly|ar|fa|ku|tzm-latn|tzm)(?:-\w{2})?)$/)) {
+  } else if (locale.match(/^((?:ar-LY|ar|fa|ku|tzm-LATN|tzm)(?:-\w{2})?)$/)) {
     return 7
   } else if (locale.match(/^((?:dv)(?:-\w{2})?)$/)) {
     return 8
--- packages/locale-weekinfo/src/minimalDays.ts
+++ packages/locale-weekinfo/src/minimalDays.ts
@@ -1,13 +1,13 @@
 /* eslint-disable */
 
 export const getMinimalDays = (locale: string): number => {
-  if (locale.match(/^((?:ar-kw|ar-ly|ar|dv|fa|ku|tzm-latn|tzm)(?:-\w{2})?)$/)) {
+  if (locale.match(/^((?:ar-KW|ar-LY|ar|dv|fa|ku|tzm-LATN|tzm)(?:-\w{2})?)$/)) {
     return 1
-  } else if (locale.match(/^((?:gom-deva|gom-latn)(?:-\w{2})?)$/)) {
+  } else if (locale.match(/^((?:gom-DEVA|gom-LATN)(?:-\w{2})?)$/)) {
     return 3
-  } else if (locale.match(/^((?:af|ar-dz|ar-ma|ar-tn|bm|br|ca|cs|cy|da|de-at|de-ch|de|el|en-au|en-gb|en-ie|en-nz|en-sg|es-do|es-mx|es|et|fi|fil|fo|fr-ch|fr|fy|ga|gd|gl|hu|is|it-ch|it|km|lb|lt|lv|mi|mt|my|nb|nl-be|nl|nn|oc-lnc|pl|pt|ru|sd|se|sk|sq|ss|sv|tet|tl-ph|tlh|tzl|ur|vi|x-pseudo|yo|zh-cn)(?:-\w{2})?)$/)) {
+  } else if (locale.match(/^((?:af|ar-DZ|ar-MA|ar-TN|bm|br|ca|cs|cy|da|de-AT|de-CH|de|el|en-AU|en-GB|en-IE|en-NZ|en-SG|en|es-DO|es-MX|es|et|fi|fil|fo|fr-CH|fr|fy|ga|gd|gl|hu|is|it-CH|it|km|lb|lt|lv|mi|mt|my|nb|nl-BE|nl|nn|oc-LNC|pl|pt|ru|sd|se|sk|sq|ss|sv|tet|tl-PH|tlh|tzl|ur|vi|x-PSEUDO|yo|zh-CN)(?:-\w{2})?)$/)) {
     return 4
-  } else if (locale.match(/^((?:az|be|bg|bs|cv|eo|eu|hr|hy-am|jv|ka|kk|ky|me|mk|ms-my|ms|ro|sl|sr-cyrl|sr|sw|tg|tk|tr|ug-cn|uk|uz-latn|uz)(?:-\w{2})?)$/)) {
+  } else if (locale.match(/^((?:az|be|bg|bs|cv|eo|eu|hr|hy-AM|jv|ka|kk|ky|me|mk|ms-MY|ms|ro|sl|sr-CYRL|sr|sw|tg|tk|tr|ug-CN|uk|uz-LATN|uz)(?:-\w{2})?)$/)) {
     return 7
   }
 
