de16f49e7781589eff88a43119246b8fbda7fc4b
Naguumo <ishbharal@gmail.com>
2021-07-14 11:53:25 -0500

    Simplify ordinals typing

--- packages/datetimeformat-tokens/src/ordinals.ts
+++ packages/datetimeformat-tokens/src/ordinals.ts
@@ -1,44 +1,11 @@
 import { localeOrdinals } from './localeOrdinals'
 import { localeOrdinalsSpecial } from './localeOrdinalsSpecial'
 
-export type SymbolOrdinals = string
-
-export type NumberOrdinals = {
-  one: string
-  two: string
-  few: string
-  other: string
-}
-
-export type SpecialOrdinals = {
-  [key: string]: string
-}
-
-export type Ordinals = SymbolOrdinals | NumberOrdinals | SpecialOrdinals
-
-export const isSymbolOrdinals = (
-  _locale: string,
-  ordinals: Ordinals
-): ordinals is SymbolOrdinals => {
-  return typeof ordinals === 'string'
-}
-
-export const isNumberOrdinals = (
-  locale: string,
-  ordinals: Ordinals
-): ordinals is NumberOrdinals => {
-  return typeof ordinals === 'object' && !isSpecialOrdinals(locale, ordinals)
-}
-
-export const isSpecialOrdinals = (
-  locale: string,
-  ordinals: Ordinals
-): ordinals is SpecialOrdinals => {
-  // Use prefix as backup if specific locale cannot be found
-  const specialsFunction =
-    localeOrdinalsSpecial[locale] ?? localeOrdinalsSpecial[locale.split('-')[0]]
-  return typeof ordinals === 'object' && specialsFunction !== undefined
-}
+export type Ordinals =
+  | string
+  | {
+      [key: string]: string
+    }
 
 export const getOrdinalForValue = (
   num: number,
@@ -50,14 +17,14 @@ export const getOrdinalForValue = (
   const ordinals: Ordinals = localeOrdinals[locale] ?? localeOrdinals[prefix]
 
   // Short circuit to avoid PluralRules call in case of string
-  if (isSymbolOrdinals(locale, ordinals)) {
+  if (typeof ordinals === 'string') {
     return ordinals
   }
 
   // Gets one of 'one', 'two', 'few', 'many', 'other'
   const count = new Intl.PluralRules(locale, { type: 'ordinal' }).select(num)
 
-  if (isSpecialOrdinals(locale, ordinals)) {
+  if (localeOrdinalsSpecial[locale] || localeOrdinalsSpecial[prefix]) {
     // In this case ordinals refers only to the data part
     // Use prefix as backup if specific locale cannot be found
     return (
@@ -66,6 +33,6 @@ export const getOrdinalForValue = (
     )
   }
 
-  // Default to a NumberOrdinal using PluralRules
+  // Default to using PluralRules
   return ordinals[count]
 }
