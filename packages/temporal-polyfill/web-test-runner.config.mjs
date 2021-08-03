f3cc66c141bc10d772a38e8784ecc158263ac555
Naguumo <ishbharal@gmail.com>
2021-08-03 15:59:13 -0500

    Add commonjs transforms (still doesn't work)

--- packages/temporal-polyfill/web-test-runner.config.mjs
+++ packages/temporal-polyfill/web-test-runner.config.mjs
@@ -1,10 +1,17 @@
+import commonjs from '@rollup/plugin-commonjs'
+
 export default {
   files: './e2e/*.mjs',
   nodeResolve: true,
+  preserveSymlinks: true,
+  esbuildTarger: 'auto',
+  plugins: [commonjs()],
 
+  watch: true,
   // If you need to manually test
   // manual: true,
+  // open: true,
 
   // Due to being a monorepo
-  // rootDir: '../../',
+  rootDir: '../../',
 }
