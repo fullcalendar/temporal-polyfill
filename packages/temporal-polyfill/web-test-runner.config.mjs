5850835f47f46130b66279703d6d59ae303b9c68
Adam Shaw <arshaw@users.noreply.github.com>
2021-08-09 15:14:25 -0400

    make tests easier to work with

--- packages/temporal-polyfill/web-test-runner.config.mjs
+++ packages/temporal-polyfill/web-test-runner.config.mjs
@@ -1,7 +1,26 @@
 import { defaultReporter } from '@web/test-runner'
 
 export default {
-  files: './e2e/*.mjs',
+  files: [ // './e2e/*.mjs'
+    // './e2e/calendar.mjs',
+    // './e2e/datemath.mjs',
+    // './e2e/duration.mjs',
+    // './e2e/ecmascript.mjs',
+    // './e2e/exports.mjs',
+    // './e2e/instant.mjs',
+    // './e2e/intl.mjs',
+    // './e2e/now.mjs',
+    // './e2e/plaindate.mjs',
+    './e2e/plaindatetime.mjs',
+    // './e2e/plainmonthday.mjs',
+    // './e2e/plaintime.mjs',
+    // './e2e/plainyearmonth.mjs',
+    // './e2e/regex.mjs',
+    // './e2e/timezone.mjs',
+    // './e2e/usercalendar.mjs',
+    // './e2e/usertimezone.mjs',
+    // './e2e/zoneddatetime.mjs',
+  ],
   nodeResolve: true,
   preserveSymlinks: true,
   esbuildTarget: 'auto',
