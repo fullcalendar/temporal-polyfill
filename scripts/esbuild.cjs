676441111bd7a06e1e0a58c167ebd76277840310
Adam Shaw <arshaw@users.noreply.github.com>
2021-09-15 16:12:46 -0400

    ability to watch

--- scripts/esbuild.cjs
+++ scripts/esbuild.cjs
@@ -1,32 +1,53 @@
 // meant to be run within a package directory
 
-const { resolve, relative } = require('path')
-const rootPath = resolve('.')
-const packageJson = require(relative(
-  __dirname,
-  resolve(rootPath, './package.json'),
-))
-
+const path = require('path')
+const esbuild = require('esbuild')
 require('colors')
 
-const { build } = require('esbuild')
+const packageJson = require(path.resolve('./package.json'))
 
 // No external dependencies bundled
 // If we DID want to bundle some, we'd need esbuild-plugin-pnp
 const external = Object.keys(packageJson.dependencies ?? {})
 
-build({
-  entryPoints: [resolve(rootPath, './src/index.ts')],
-  outfile: resolve(rootPath, './dist/index.js'),
+buildFile({
+  entryPoints: ['./src/index.ts'], // relative to cwd
+  outfile: './dist/index.js', // relative to cwd
   bundle: true,
   format: 'esm',
   external,
-})
-  .then(() => {
-    console.log('Main file built'.green)
-  })
-  .catch((err) => {
-    console.warn('Building main file failed'.red)
-    console.warn(err)
-    process.exit(1)
-  })
+}, false) // watch?
+
+function buildFile(esbuildSettings, watch) {
+  const { outfile } = esbuildSettings
+  if (!outfile) {
+    throw new Error('esbuild settings much specify outfile')
+  }
+
+  if (watch) {
+    console.log(`Watch-building ${outfile}...`.green)
+    esbuildSettings = {
+      ...esbuildSettings,
+      watch: {
+        onRebuild(error) {
+          if (error) {
+            console.error(`Error building ${outfile}`, error)
+          } else {
+            console.log(`Rebuilt ${outfile}`.green)
+          }
+        },
+      },
+    }
+  }
+
+  let promise = esbuild.build(esbuildSettings)
+  if (!watch) {
+    promise = promise.then(() => {
+      console.log(`Built ${outfile}`.green)
+    }, (error) => {
+      console.error(`Error building ${outfile}`, error)
+    })
+  }
+
+  return promise
+}
