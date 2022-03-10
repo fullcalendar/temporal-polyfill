0115558bebe5d609ea3b618aa5fa5b996f2a1d42
Adam Shaw <arshaw@users.noreply.github.com>
2022-03-10 12:08:56 -0500

    fix intl problem with japanese/islamic

--- packages/temporal-polyfill/src/calendarImpl/bugs.ts
+++ packages/temporal-polyfill/src/calendarImpl/bugs.ts
@@ -1,5 +1,6 @@
 import { isoToEpochMilli } from '../dateUtils/isoMath'
 import { milliInDay } from '../dateUtils/units'
+import { queryCalendarImpl } from './calendarImplQuery'
 import { IntlCalendarImpl } from './intlCalendarImpl'
 
 // https://bugs.chromium.org/p/chromium/issues/detail?id=1173158
@@ -30,9 +31,13 @@ function hasEpochMilliBug(calendarID: string) {
     if (goodEpochMilli === undefined) {
       hasBug = false
     } else {
+      let impl = queryCalendarImpl(calendarID)
+
+      // HACK
       // Even if Japanese, must leverage Intl.DateTimeFormat, so force IntlCalendarImpl
-      // TODO: Not efficient. Creates an extra DateTimeFormat.
-      const impl = new IntlCalendarImpl(calendarID)
+      if (!(impl instanceof IntlCalendarImpl)) {
+        impl = new IntlCalendarImpl(calendarID)
+      }
 
       const badEpochMilli = goodEpochMilli - milliInDay
       const fields = impl.computeFields(badEpochMilli)
