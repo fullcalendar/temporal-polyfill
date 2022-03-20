b666b879a207cbe9ac01461c92949bf54087d953
Adam Shaw <arshaw@users.noreply.github.com>
2022-03-20 15:42:05 -0400

    update specialCases to new types

--- packages/temporal-polyfill/src/timeZoneImpl/specialCases.ts
+++ packages/temporal-polyfill/src/timeZoneImpl/specialCases.ts
@@ -5,9 +5,11 @@ export const specialCases: {
 } = {
   'Pacific/Apia': {
     2011: [
-      [1301752800, -36000, -39600], // start DST
-      [1316872800, -39600, -36000], // end DST
-      [1325239200, -36000, 50400], // change of time zone
+      // TODO: this was much nicer when specified in seconds
+      // TODO: have RawTransitions be seconds again?
+      [1301752800000000000n, -36000000000000, -39600000000000], // start DST
+      [1316872800000000000n, -39600000000000, -36000000000000], // end DST
+      [1325239200000000000n, -36000000000000, 50400000000000], // change of time zone
     ],
   },
 }
