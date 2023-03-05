72e43a44becab7310c30b64ee153e4c22fa873c0
Adam Shaw <arshaw@users.noreply.github.com>
2023-03-05 17:11:21 -0500

    fix how test timeout works

--- packages/temporal-polyfill/runtest262.js
+++ packages/temporal-polyfill/runtest262.js
@@ -1,16 +1,15 @@
 import runTest262 from '@js-temporal/temporal-test262-runner'
 
+const testGlobs = process.argv.slice(2)
+
 const result = runTest262({
   test262Dir: 'test262',
   polyfillCodeFile: 'dist/global.js',
   expectedFailureFiles: ['expected-failures.txt'],
-  testGlobs: process.argv.slice(2),
-
-  // We'd like to set a high timeout when debugging, HOWEVER, some of the tests reveal infinite
-  // loops in our code, so they're allowed to run for a whole minute.
-  // TODO: fix infinite loops.
-  //
-  // timeoutMsecs: 60000,
+  testGlobs,
+  timeoutMsecs: testGlobs.length
+    ? 3600000 // (1 hour) running specific tests, don't timeout while debugging
+    : 2000, // (2 seconds) running ALL tests, don't wait for infinite loops
 })
 
 // if result is `true`, all tests succeeded
