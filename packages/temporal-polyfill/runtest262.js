f3e0fcdebcb0ecdfbcf5c060e352725c4c62a568
Adam Shaw <arshaw@users.noreply.github.com>
2023-03-05 16:49:15 -0500

    make debugging runner better

--- packages/temporal-polyfill/runtest262.js
+++ packages/temporal-polyfill/runtest262.js
@@ -5,6 +5,12 @@ const result = runTest262({
   polyfillCodeFile: 'dist/global.js',
   expectedFailureFiles: ['expected-failures.txt'],
   testGlobs: process.argv.slice(2),
+
+  // We'd like to set a high timeout when debugging, HOWEVER, some of the tests reveal infinite
+  // loops in our code, so they're allowed to run for a whole minute.
+  // TODO: fix infinite loops.
+  //
+  // timeoutMsecs: 60000,
 })
 
 // if result is `true`, all tests succeeded
