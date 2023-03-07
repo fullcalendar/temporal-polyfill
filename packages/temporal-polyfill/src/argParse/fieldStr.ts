56e5dc850b7d94a7f1e38e561b2c9aedcd506747
Adam Shaw <arshaw@users.noreply.github.com>
2023-03-07 16:25:12 -0500

    parse reject infinite numbers

--- packages/temporal-polyfill/src/argParse/fieldStr.ts
+++ packages/temporal-polyfill/src/argParse/fieldStr.ts
@@ -3,33 +3,44 @@ import { durationUnitNames } from './unitStr'
 
 export const yearMonthFieldMap = {
   era: String,
-  eraYear: Number,
-  year: Number,
-  month: Number,
+  eraYear: refineNumber,
+  year: refineNumber,
+  month: refineNumber,
   monthCode: String,
 }
 
 export const dateFieldMap = {
   ...yearMonthFieldMap,
-  day: Number,
+  day: refineNumber,
 }
 
 export const timeFieldMap = {
-  hour: Number,
-  minute: Number,
-  second: Number,
-  millisecond: Number,
-  microsecond: Number,
-  nanosecond: Number,
+  hour: refineNumber,
+  minute: refineNumber,
+  second: refineNumber,
+  millisecond: refineNumber,
+  microsecond: refineNumber,
+  nanosecond: refineNumber,
 }
 
 export const monthDayFieldMap = {
   era: String,
-  eraYear: Number,
-  year: Number,
-  month: Number,
+  eraYear: refineNumber,
+  year: refineNumber,
+  month: refineNumber,
   monthCode: String,
-  day: Number,
+  day: refineNumber,
+}
+
+// TODO: more DRY with constrainInt
+function refineNumber(input: any): number {
+  const num = Number(input)
+
+  if (!Number.isFinite(num)) {
+    throw new RangeError('Number must be finite')
+  }
+
+  return num
 }
 
 export const durationFieldMap = strArrayToHash(durationUnitNames, () => Number)
