3729223655f9df0ea59f355baeb30acd94e51819
Adam Shaw <arshaw@users.noreply.github.com>
2023-03-19 19:18:53 -0400

    testing improvements

--- packages/temporal-polyfill/runtest262.js
+++ packages/temporal-polyfill/runtest262.js
@@ -1,6 +1,11 @@
 import runTest262 from '@js-temporal/temporal-test262-runner'
 
 const testGlobs = process.argv.slice(2)
+const noTimeoutIndex = testGlobs.indexOf('--no-timeout')
+const noTimeout = noTimeoutIndex !== -1
+if (noTimeout) {
+  testGlobs.splice(noTimeoutIndex, 1)
+}
 
 const result = runTest262({
   test262Dir: 'test262',
@@ -8,17 +13,14 @@ const result = runTest262({
   expectedFailureFiles: [
     // from https://github.com/js-temporal/temporal-polyfill/blob/main/runtest262.mjs
     'misc/expected-failures.txt',
-    'misc/expected-failures-es5.txt',
-    // 'misc/expected-failures-opt.txt',
+    // 'misc/expected-failures-es5.txt',
+    'misc/expected-failures-opt.txt',
     'misc/expected-failures-before-node18.txt',
     // 'misc/expected-failures-before-node16.txt',
-    'misc/expected-failures-todo-migrated-code.txt',
+    // 'misc/expected-failures-todo-migrated-code.txt',
   ],
   testGlobs,
-  // timeoutMsecs: 2000,
-  timeoutMsecs: testGlobs.length
-    ? 3600000 // (1 hour) running specific tests, don't timeout while debugging
-    : 2000, // (2 seconds) running ALL tests, don't wait for infinite loops
+  timeoutMsecs: (noTimeout ? (24 * 60 * 60) : 2) * 1000,
 })
 
 // if result is `true`, all tests succeeded
