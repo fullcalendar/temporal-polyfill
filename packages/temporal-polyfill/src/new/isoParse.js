dadfe33c00486c564e3605251dc0f91356ce7d7b
Adam Shaw <arshaw@users.noreply.github.com>
2023-07-06 15:51:19 -0400

    parsing for datetimes

--- packages/temporal-polyfill/src/new/isoParse.js
+++ packages/temporal-polyfill/src/new/isoParse.js
@@ -1,4 +1,6 @@
+import { nanoInSecond } from '../dateUtils/units'
 import { isoCalendarId } from './calendarConfig'
+import { queryCalendarImpl } from './calendarImpl'
 import {
   pluckIsoDateInternals,
   pluckIsoDateTimeInternals,
@@ -9,45 +11,40 @@ import {
   constrainIsoDateTimeInternals,
   constrainIsoTimeFields,
   isoToEpochNano,
+  nanoToIsoTimeAndDay,
 } from './isoMath'
+import { queryTimeZoneImpl } from './timeZoneImpl'
 import { getMatchingInstantFor, utcTimeZoneId } from './timeZoneOps'
-
-// TODO: finish isoParse
+import { nanoInHour, nanoInMinute } from './units'
 
 // High-level
 // -------------------------------------------------------------------------------------------------
 
 export function parseInstant(s) {
   const parsed = parseDateTime(s)
-
-  if (parsed) {
-    let offsetNano
-
-    if (parsed.hasZ) {
-      offsetNano = 0
-    } else if (parsed.offset) {
-      offsetNano = parseOffsetNano(parsed.offset)
-    } else {
-      return new RangeError()
-    }
-
-    return isoToEpochNano(parsed).addNumber(offsetNano)
+  if (!parsed) {
+    throw new RangeError()
   }
 
-  throw new RangeError()
+  let offsetNano
+
+  if (parsed.hasZ) {
+    offsetNano = 0
+  } else if (parsed.offset) {
+    offsetNano = parseOffsetNano(parsed.offset)
+  } else {
+    return new RangeError()
+  }
+
+  return isoToEpochNano(parsed).addNumber(offsetNano)
 }
 
 export function parseZonedDateTime(s) {
-  const parsed = parseDateTime(s) // TODO: use just 'calendar' and 'timeZone' ?
-
-  if (parsed) {
-    if (!parsed.timeZone) {
-      throw new RangeError()
-    }
-    return processZonedDateTimeParse(parsed)
+  const parsed = parseDateTime(s)
+  if (!parsed || !parsed.timeZone) {
+    throw new RangeError()
   }
-
-  throw new RangeError()
+  return processZonedDateTimeParse(parsed)
 }
 
 export function processZonedDateTimeParse(parsed) {
@@ -60,7 +57,6 @@ export function processZonedDateTimeParse(parsed) {
     'compatible',
     true, // fuzzy
   )
-
   return {
     epochNanoseconds,
     timeZone: parsed.timeZone,
@@ -70,52 +66,26 @@ export function processZonedDateTimeParse(parsed) {
 
 export function parsePlainDateTime(s) {
   const parsed = parseDateTime(s)
-  if (parsed) {
-    return pluckIsoDateTimeInternals(parsed)
+  if (!parsed) {
+    throw new RangeError()
   }
-
-  throw new RangeError()
+  return pluckIsoDateTimeInternals(parsed)
 }
 
 export function parsePlainDate(s) {
   const parsed = parseDateTime(s)
-  if (parsed) {
-    return parsed
+  if (!parsed) {
+    throw new RangeError()
   }
-
-  throw new RangeError()
+  return pluckIsoDateInternals(parsed)
 }
 
 export function parsePlainYearMonth(s) {
-  let parsed = parseYearMonth(s)
-  if (!parsed) {
-    parsed = parseDateTime(s)
-    if (parsed) {
-      parsed = pluckIsoDateInternals(parsed)
-    }
-  }
-
-  if (parsed) {
-    return parsed
-  }
-
-  throw new RangeError()
+  return parseYearMonth(s) || parsePlainDate(s) // parsePlainDate will throw error
 }
 
 export function parsePlainMonthDay(s) {
-  let parsed = parseMonthDay(s)
-  if (!parsed) {
-    parsed = parseDateTime(s)
-    if (parsed) {
-      parsed = pluckIsoDateInternals(parsed)
-    }
-  }
-
-  if (parsed) {
-    return parsed
-  }
-
-  throw new RangeError()
+  return parseMonthDay(s) || parsePlainDate(s) // parsePlainDate will throw error
 }
 
 export function parsePlainTime(s) {
@@ -123,31 +93,28 @@ export function parsePlainTime(s) {
 
   if (!parsed) {
     parsed = parseDateTime(s)
-
     if (parsed && !parsed.hasTime) {
       throw new RangeError()
     }
   }
 
-  if (parsed) {
-    if (parsed.hasZ) {
-      throw new RangeError()
-    }
-    if (parsed.calendar !== undefined && parsed.calendar !== isoCalendarId) {
-      throw new RangeError()
-    }
-
-    if (parseMonthDay(s)) {
-      throw new RangeError()
-    }
-    if (parseYearMonth(s)) {
-      throw new RangeError()
-    }
-
-    return pluckIsoTimeFields(parsed)
+  if (!parsed) {
+    throw new RangeError()
+  }
+  if (parsed.hasZ) {
+    throw new RangeError()
+  }
+  if (parsed.calendar !== undefined && parsed.calendar.id !== isoCalendarId) {
+    throw new RangeError()
+  }
+  if (parseMonthDay(s)) {
+    throw new RangeError()
+  }
+  if (parseYearMonth(s)) {
+    throw new RangeError()
   }
 
-  throw new RangeError()
+  return pluckIsoTimeFields(parsed)
 }
 
 export function parseCalendarId(s) {
@@ -156,7 +123,6 @@ export function parseCalendarId(s) {
       parseDateTime(s) || parseYearMonth(s) || parseMonthDay(s)
     )?.calendar.id || isoCalendarId
   }
-
   return s
 }
 
@@ -177,49 +143,186 @@ export function parseTimeZoneId(s) {
   return s
 }
 
-// Low-level
-// -------------------------------------------------------------------------------------------------
-
-function parseDateTime(s) {
-  return constrainIsoDateTimeInternals({
-    // { isYear, isoMonth, isoDay,
-    //   isoHour, isMinute, isoSecond, etc...
-    //   hasTime, hasZ, offset,
-    //   calendar, timeZone }
-    //
-    // should use `queryCalendarOps(parsed.calendar || isoCalendarId)`
-    // should use `queryTimeZoneOps(parsed.timeZone)`
-  })
-}
-
-function parseYearMonth(s) {
-  return constrainIsoDateInternals({
-    // { isYear, isoMonth, isoDay
-    //   calendar }
-    //
-    // should use `queryCalendarOps(parsed.calendar || isoCalendarId)`
-  })
-}
-
-function parseMonthDay(s) {
-  return constrainIsoDateInternals({
-    // { isYear, isoMonth, isoDay
-    //   calendar }
-    //
-    // should use `queryCalendarOps(parsed.calendar || isoCalendarId)`
-  })
-}
-
-function parseTime(s) {
-  return constrainIsoTimeFields({
-    // { isoHour, isoMinute, isoSecond, etc... }
-  })
-}
-
 export function parseOffsetNano(s) {
-  // number
+  const parts = numericOffsetRegExp.exec(normalizeDashes(s))
+  return parts && parseOffsetParts(parts.slice(1))
 }
 
 export function parseDuration(s) {
   // includes sign
 }
+
+// Low-level
+// -------------------------------------------------------------------------------------------------
+
+const yearMonthRegExpStr =
+  '([+-]\\d{6}|\\d{4})' + // 0:year
+  '-?(\\d{2})' // 1:month
+  // 2:annotations
+
+const dateRegExpStr =
+  yearMonthRegExpStr + // 0:year, 1:month
+  '-?(\\d{2})' // 2:day
+  // 3:annotations
+
+const monthDayRegExpStr =
+  '(--)?(\\d{2})' + // 1:month
+  '-?(\\d{2})' // 2:day
+  // 3:annotations
+
+const numericTimeRegExpStr =
+  '(\\d{2})' + // 0:hour
+  '(:?(\\d{2})' + // 2:minute (NOTE: ':?' means optional ':')
+  '(:?(\\d{2})' + // 4:second
+  '([.,](\\d{1,9}))?' + // 6:afterDecimal
+  ')?)?'
+
+const numericOffsetRegExpStr =
+  '([+-])' + // 0:plusOrMinus
+  numericTimeRegExpStr // 1:hour, 3:minute, 5:second, 7:afterDecimal
+
+const offsetRegExpStr =
+  '(Z|' + // 0:zOrOffset
+  numericOffsetRegExpStr + // 1:plusOrMinus, 2:hour, 4:minute, 6:second, 8:afterDecimal
+  ')?'
+
+const timeRegExpStr =
+  numericTimeRegExpStr + // 0:hour, 2:minute, 4:second, 6:afterDecimal
+  offsetRegExpStr // 7:zOrOffset, 8:plusOrMinus, 9:hour, 11:minute, 13:second, 15:afterDecimal
+  // 16:annotations
+
+const dateTimeRegExpStr =
+  dateRegExpStr + // 0:year, 1:month, 2:day
+  '([T ]' + // 3:timeEverything
+  timeRegExpStr +
+  // 4:hour, 6:minute, 8:second, 10:afterDecimal
+  // 11:zOrOffset, 12:plusOrMinus, 13:hour, 15:minute, 17:second, 19:afterDecimal
+  ')?'
+  // 20:annotations
+
+const annotationRegExpStr = '((\\[[^\\]]*\\])*)'
+
+const yearMonthRegExp = createRegExp(yearMonthRegExpStr + annotationRegExpStr)
+const monthDayRegExp = createRegExp(monthDayRegExpStr + annotationRegExpStr)
+const dateTimeRegExp = createRegExp(dateTimeRegExpStr + annotationRegExpStr)
+const timeRegExp = createRegExp('T?' + timeRegExpStr + annotationRegExpStr)
+const numericOffsetRegExp = createRegExp(numericOffsetRegExpStr) // annotations not allowed
+
+function parseDateTime(s) {
+  const parts = dateTimeRegExp.exec(normalizeDashes(s)) // 0 is whole-match
+  return parts && constrainIsoDateTimeInternals({
+    isoYear: parseInt1(parts[1]),
+    isoMonth: parseInt1(parts[2]),
+    isoDay: parseInt1(parts[3]),
+    ...parseTimeParts(parts.slice(5)), // parses annotations
+  })
+}
+
+function parseYearMonth(s) {
+  const parts = yearMonthRegExp.exec(normalizeDashes(s)) // 0 is whole-match
+  return parts && constrainIsoDateInternals({
+    isoYear: parseInt1(parts[1]),
+    isoMonth: parseInt1(parts[2]),
+    isoDay: 1,
+    ...parseAnnotations(parts[3]),
+  })
+}
+
+function parseMonthDay(s) {
+  const parts = monthDayRegExp.exec(normalizeDashes(s)) // 0 is whole-match
+  return parts && constrainIsoDateInternals({
+    isoYear: parseInt1(parts[0]),
+    isoMonth: parseInt1(parts[1]),
+    isoDay: 1,
+    ...parseAnnotations(parts[2]),
+  })
+}
+
+function parseTime(s) {
+  const parts = timeRegExp.exec(normalizeDashes(s)) // 0 is whole-match
+  return constrainIsoTimeFields(parseTimeParts(parts.slice(1)))
+}
+
+function parseTimeParts(parts) { // parses annotations
+  const isoSecond = parseInt0(parts[4])
+  return {
+    ...nanoToIsoTimeAndDay(parseNanoAfterDecimal(parts[6] || ''))[0],
+    isoHour: parseInt0(parts[0]),
+    isoMinute: parseInt0(parts[2]),
+    isoSecond: isoSecond === 60 ? 59 : isoSecond, // massage leap-second
+    ...parseAnnotations(parts[16]),
+  }
+}
+
+function parseOffsetParts(parts) {
+  return (parts[0] === '+' ? 1 : -1) * (
+    parseInt0(parts[0]) * nanoInHour +
+    parseInt0(parts[2]) * nanoInMinute +
+    parseInt0(parts[4]) * nanoInSecond +
+    parseNanoAfterDecimal(parts[6] || '')
+  )
+}
+
+function parseNanoAfterDecimal(str) {
+  return parseInt(str.padEnd(9, '0'))
+}
+
+function parseAnnotations(s) {
+  let calendarId
+  let timeZoneId
+
+  for (const chunk of s.split(']')) {
+    if (chunk) { // not the empty end chunk
+      let annotation = chunk.slice(1) // remove leading '['
+      let isCritical = false
+
+      if (annotation.charAt(0) === '!') {
+        isCritical = true
+        annotation = annotation.slice(1)
+      }
+
+      const annotationParts = annotation.split('=')
+      if (annotationParts.length === 1) {
+        if (timeZoneId !== undefined) {
+          throw new RangeError('Cannot specify timeZone multiple times')
+        }
+        timeZoneId = annotation
+      } else if (annotationParts[0] === 'u-ca') {
+        if (calendarId === undefined) { // ignore subsequent calendar annotations
+          calendarId = annotationParts[1]
+        }
+      } else if (isCritical) {
+        throw new RangeError(`Critical annotation '${annotationParts[0]}' not used`)
+      }
+    }
+  }
+
+  return {
+    calendar: queryCalendarImpl(calendarId || isoCalendarId),
+    timeZone: timeZoneId ? queryTimeZoneImpl(timeZoneId) : undefined,
+  }
+}
+
+const unicodeDashRegExp = /\u2212/g
+
+function normalizeDashes(str) {
+  return str.replace(unicodeDashRegExp, '-')
+}
+
+function createRegExp(meat) {
+  return new RegExp(`^${meat}$`, 'i')
+}
+
+function parseIntWithDefault(defaultInt, s) {
+  if (s === undefined) {
+    return defaultInt
+  }
+  const n = parseInt(s)
+  if (Object.is(n, -0)) {
+    throw RangeError('no negative zero')
+  }
+  return n
+}
+
+const parseInt0 = parseIntWithDefault.bind(undefined, 0)
+const parseInt1 = parseIntWithDefault.bind(undefined, 1)
