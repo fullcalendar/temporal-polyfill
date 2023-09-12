cd250c95814f046804be8045f7da0eb130cc4be4
Adam Shaw <arshaw@users.noreply.github.com>
2023-09-12 12:06:13 -0400

    remove old src

--- packages/temporal-polyfill/src/old/impl.build.ts
+++
@@ -1,9 +0,0 @@
-import type * as Spec from 'temporal-spec'
-import { toTemporalInstant as toTemporalInstantImpl } from './native/date'
-import { DateTimeFormat } from './native/intlTemporal'
-import { Temporal as TemporalImpl } from './public/temporal'
-import { getGlobalThis } from './utils/dom'
-
-export const Temporal: typeof Spec.Temporal = TemporalImpl
-export const Intl: typeof Spec.Intl = { ...getGlobalThis().Intl, DateTimeFormat }
-export const toTemporalInstant = toTemporalInstantImpl
