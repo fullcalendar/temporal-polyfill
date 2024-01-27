1a2aa5f55f9603b4de2e64dd5f5caccf881dfab4
Adam Shaw <arshaw@users.noreply.github.com>
2024-01-27 18:42:02 -0500

    clean export-size-output, better cleaning

--- packages/temporal-polyfill/scripts/clean.js
+++ packages/temporal-polyfill/scripts/clean.js
@@ -1,29 +1,30 @@
 #!/usr/bin/env node
 
+// TODO: clean export-size-output
+
 import fs from 'fs/promises'
 import path from 'path'
 
-const deleteTsc = process.argv.slice(2).includes('--all')
-
-const dirPath = './dist'
-const whitelist = [
-  '.npmignore',
-  ...(deleteTsc ? [] : [
-    '.tsc',
-    'tsconfig.tsbuildinfo'
-  ])
-]
+const shouldDeleteTsc = process.argv.slice(2).includes('--tsc')
 
 async function clean() {
-  const files = await fs.readdir(dirPath)
+  const files = await fs.readdir('./dist')
 
   for (const file of files) {
-    const filePath = path.join(dirPath, file)
+    const filePath = path.join('./dist', file)
 
-    if (!whitelist.includes(file)) {
+    if (
+      file !== '.npmignore' &&
+      (shouldDeleteTsc || (
+        file !== '.tsc' &&
+        file !== 'tsconfig.tsbuildinfo'
+      ))
+    ) {
       await fs.rm(filePath, { recursive: true })
     }
   }
+
+  await fs.rm('./export-size-output', { recursive: true, force: true })
 }
 
 clean()
