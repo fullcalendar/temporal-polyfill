e988f71ffc420beacc3b7d4eea944f4bb312804a
Adam Shaw <arshaw@users.noreply.github.com>
2024-01-27 19:18:08 -0500

    can test esm bundle

--- packages/temporal-polyfill/scripts/test.js
+++ packages/temporal-polyfill/scripts/test.js
@@ -5,6 +5,9 @@ import runTest262 from '@js-temporal/temporal-test262-runner'
 import { join as joinPaths } from 'path'
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
+import { rollup } from 'rollup'
+import terser from '@rollup/plugin-terser'
+import { extensions } from './config.js'
 
 const scriptsDir = joinPaths(process.argv[1], '..')
 const pkgDir = joinPaths(scriptsDir, '..')
@@ -14,33 +17,38 @@ yargs(hideBin(process.argv))
   .command(
     '*',
     'Run test262 tests',
-    (builder) => {
-      builder.option('update', {
+    (builder) => builder
+      .option('update', {
         requiresArg: false,
         default: false,
         type: 'boolean',
         description: 'Whether to update the existing expected-failure files on-disk and remove tests that now pass.'
       })
-      builder.option('timeout', {
+      .option('timeout', {
         requiresArg: false,
         default: 30000,
         type: 'number',
         description: 'Millisecond allowance for a single test file to run'
       })
-      builder.option('max', {
+      .option('max', {
         requiresArg: false,
         default: 10,
         type: 'number',
         description: 'Maxiumum allowed number of failures before aborting'
       })
-      builder.option('min', { // for "minify"
+      .option('esm', {
+        requiresArg: false,
+        default: false,
+        type: 'boolean',
+        description: 'Whether to test the ESM module'
+      })
+      .option('min', { // for "minify"
         requiresArg: false,
         default: false,
         type: 'boolean',
         description: 'Whether to test the minified bundle'
-      })
-    },
-    (parsedArgv) => {
+      }),
+    async (options) => {
       const expectedFailureFiles = [
         'expected-failures.txt',
         'expected-failures-surface.txt',
@@ -58,27 +66,52 @@ yargs(hideBin(process.argv))
         expectedFailureFiles.push('expected-failures-before-node16.txt')
       }
 
-      console.log(`Testing with Node v${nodeVersion}...`)
+      let polyfillPath // from package root
+
+      if (options.esm) {
+        console.log('Bundling ESM...')
+        polyfillPath = await bundleEsm('./dist', 'global', options.min)
+      } else if (options.min) {
+        polyfillPath = './dist/global' + extensions.iifeMin
+      } else {
+        polyfillPath = './dist/global' + extensions.iife
+      }
+
+      console.log(`Testing ${polyfillPath} with Node v${nodeVersion}...`)
 
       const result = runTest262({
         test262Dir: joinPaths(monorepoDir, 'test262'),
-        polyfillCodeFile: joinPaths(
-          pkgDir,
-          parsedArgv.min
-            ? 'dist/global.min.js'
-            : 'dist/global.js'
-        ),
+        polyfillCodeFile: joinPaths(pkgDir, polyfillPath),
         expectedFailureFiles: expectedFailureFiles.map((filename) => (
           joinPaths(scriptsDir, 'test-config', filename)
         )),
-        testGlobs: parsedArgv._,
-        timeoutMsecs: parsedArgv.timeout || 86400000,
-        updateExpectedFailureFiles: parsedArgv.update,
-        maxFailures: parsedArgv.max,
+        testGlobs: options._,
+        timeoutMsecs: options.timeout || 86400000,
+        updateExpectedFailureFiles: options.update,
+        maxFailures: options.max,
         fullPath: true
       })
 
       process.exit(result ? 0 : 1)
     }
   )
+  .showHelpOnFail(false)
   .help().argv
+
+async function bundleEsm(dir, exportName, minify) {
+  const inputFilename = exportName + extensions.esm
+  const outputFilename = '.bundled.' + (minify ? 'min.' : '') + inputFilename
+
+  const bundle = await rollup({
+    input: dir + '/' + inputFilename,
+  })
+  await bundle.write({
+    file: dir + '/' + outputFilename,
+    format: 'iife',
+    plugins: [
+      minify && terser()
+    ]
+  })
+
+  return dir + '/' + outputFilename
+}
