23c70257583166aace5c63cb5917c58c46541e68
Adam Shaw <arshaw@users.noreply.github.com>
2024-02-29 09:46:04 -0500

    forgot relativeSystem.ts

---
+++ packages/temporal-polyfill/src/internal/relativeSystem.ts
@@ -0,0 +1,52 @@
+import { BigNano } from './bigNano'
+import {
+  IsoDateFields,
+  IsoDateTimeFields,
+  isoTimeFieldDefaults,
+} from './isoFields'
+import { DateSlots, ZonedEpochSlots } from './slots'
+import { isoToEpochNano } from './timeMath'
+import { TimeZoneOps } from './timeZoneOps'
+
+// the "origin"
+export type RelativeToSlots<C, T> = DateSlots<C> | ZonedEpochSlots<C, T>
+
+// the "origin", returned from bag refining
+export type RelativeToSlotsNoCalendar<T> =
+  | IsoDateFields
+  | { epochNanoseconds: BigNano; timeZone: T }
+
+// a date marker that's moved away from the "origin"
+export type RelativeMarkerSlots =
+  | IsoDateTimeFields
+  | ZonedEpochSlots<unknown, unknown>
+
+export type RelativeSystem<CO> = [RelativeMarkerSlots, CO, TimeZoneOps?]
+
+export function createRelativeSystem<C, CO, T>(
+  getCalendarOps: (calendarSlot: C) => CO,
+  getTimeZoneOps: (timeZoneSlot: T) => TimeZoneOps,
+  slots: RelativeToSlots<C, T>,
+): RelativeSystem<CO> {
+  const calendarOps = getCalendarOps(slots.calendar)
+
+  if ((slots as ZonedEpochSlots<C, T>).epochNanoseconds) {
+    return [
+      slots as ZonedEpochSlots<C, T>,
+      calendarOps,
+      getTimeZoneOps((slots as ZonedEpochSlots<C, T>).timeZone),
+    ]
+  }
+
+  return [{ ...slots, ...isoTimeFieldDefaults }, calendarOps]
+}
+
+export function relativeMarkerToEpochNano(
+  marker: RelativeMarkerSlots,
+  timeZoneOps?: TimeZoneOps,
+): BigNano {
+  if (timeZoneOps) {
+    return (marker as ZonedEpochSlots<unknown, unknown>).epochNanoseconds
+  }
+  return isoToEpochNano(marker as IsoDateTimeFields)!
+}
