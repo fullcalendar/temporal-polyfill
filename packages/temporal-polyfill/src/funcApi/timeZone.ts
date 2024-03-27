2f1e1222ea5568e6a1f3c63be1a2ce4350b22b7e
Adam Shaw <arshaw@users.noreply.github.com>
2024-03-27 18:02:43 -0400

    TimeZoneFns

---
+++ packages/temporal-polyfill/src/funcApi/timeZone.ts
@@ -0,0 +1,33 @@
+import { createInstantSlots } from '../internal/slots'
+import { getTimeZoneEssence, refineTimeZoneId } from '../internal/timeZoneId'
+import { queryNativeTimeZone } from '../internal/timeZoneNative'
+import { bindArgs } from '../internal/utils'
+import * as InstantFns from './instant'
+import * as PlainDateTimeFns from './plainDateTime'
+
+export function getPossibleInstantsFor(
+  timeZoneId: string,
+  plainDateTime: PlainDateTimeFns.Record,
+): InstantFns.Record[] {
+  const timeZoneOps = queryNativeTimeZone(refineTimeZoneId(timeZoneId))
+  return timeZoneOps
+    .getPossibleInstantsFor(plainDateTime)
+    .map(createInstantSlots)
+}
+
+function getTransition(
+  dir: 1 | -1,
+  timeZoneId: string,
+  instant: InstantFns.Record,
+): InstantFns.Record | null {
+  const timeZoneOps = queryNativeTimeZone(refineTimeZoneId(timeZoneId))
+  const epochNano = timeZoneOps.getTransition(instant.epochNanoseconds, dir)
+  return epochNano ? createInstantSlots(epochNano) : null
+}
+
+export const getNextTransition = bindArgs(getTransition, 1)
+export const getPrevTransition = bindArgs(getTransition, -1)
+
+export function equals(timeZoneId0: string, timeZoneId1: string): boolean {
+  return getTimeZoneEssence(timeZoneId0) === getTimeZoneEssence(timeZoneId1)
+}
