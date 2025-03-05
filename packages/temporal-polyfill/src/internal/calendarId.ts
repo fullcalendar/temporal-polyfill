f64c390168a3b815b59bd304a892de3fdb9fe4c2
Adam Shaw <arshaw@users.noreply.github.com>
2025-03-05 16:39:09 -0500

    Node 16 workaround for islamic-rgsa

--- packages/temporal-polyfill/src/internal/calendarId.ts
+++ packages/temporal-polyfill/src/internal/calendarId.ts
@@ -10,7 +10,13 @@ export function refineCalendarId(id: string): string {
 export function resolveCalendarId(id: string): string {
   id = id.toLowerCase() // normalize
 
-  if (id !== isoCalendarId && id !== gregoryCalendarId) {
+  if (
+    id !== isoCalendarId &&
+    id !== gregoryCalendarId &&
+    // Node 16 and lower don't recognize islamic-rgsa and instead normalize
+    // it to islamic. Whitelist
+    id !== 'islamic-rgsa'
+  ) {
     const canonId = queryCalendarIntlFormat(id).resolvedOptions().calendar
 
     if (computeCalendarIdBase(id) !== computeCalendarIdBase(canonId)) {
