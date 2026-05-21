d7008b01952c3389d924e6c22a7eea039abcdc29
Adam Shaw <arshaw@users.noreply.github.com>
2026-05-21 12:58:22 -0400

    additional calendar utils

--- packages/temporal-polyfill/src/funcApi/calendar.ts
+++ packages/temporal-polyfill/src/funcApi/calendar.ts
@@ -1,3 +1,8 @@
+import { throwExoticCalendarError } from '../internal/calendarSlot'
+import {
+  gregoryCalendarId,
+  isoCalendarId,
+} from '../internal/intlCalendarConfig'
 import * as Native from './native/calendar'
 import { Temporal } from './nativeSwitch'
 import * as Shim from './shim/calendar'
@@ -5,9 +10,31 @@ import * as Shim from './shim/calendar'
 export const getIsoCalendar = Temporal
   ? Native.getIsoCalendar
   : Shim.getIsoCalendar
+
 export const getGregoryCalendar = Temporal
   ? Native.getGregoryCalendar
   : Shim.getGregoryCalendar
+
 export const getIntlCalendar = Temporal
   ? Native.getIntlCalendar
   : Shim.getIntlCalendar
+
+export function getCoreCalendar(calendarId: string) {
+  if (calendarId === isoCalendarId) {
+    return getIsoCalendar()
+  }
+  if (calendarId === gregoryCalendarId) {
+    return getGregoryCalendar()
+  }
+  throwExoticCalendarError()
+}
+
+export function getAnyCalendar(calendarId: string) {
+  if (calendarId === isoCalendarId) {
+    return getIsoCalendar()
+  }
+  if (calendarId === gregoryCalendarId) {
+    return getGregoryCalendar()
+  }
+  return getIntlCalendar(calendarId)
+}
