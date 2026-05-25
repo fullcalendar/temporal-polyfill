f142a2a272ced84b4e0c7abb074f626dd706548e
Adam Shaw <arshaw@users.noreply.github.com>
2026-05-25 15:58:50 -0400

    make ponyfill use native if available

--- packages/temporal-polyfill/src/classApi/core/index.ts
+++ packages/temporal-polyfill/src/classApi/core/index.ts
@@ -1,1 +1,11 @@
-export * from './implementation'
+import { NativeTemporal } from '../../nativeSwitch'
+import * as Impl from './implementation'
+
+export const Temporal = NativeTemporal || Impl.Temporal
+
+const IntlExport = NativeTemporal ? Intl : Impl.Intl
+export { IntlExport as Intl }
+
+export const toTemporalInstant = NativeTemporal
+  ? (Date.prototype as any).toTemporalInstant
+  : Impl.toTemporalInstant
